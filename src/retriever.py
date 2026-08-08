0fa759176904fbe8e2c0278ba4c5f4d96a4c63c5
Ajay Rajawat <ajayrajawat960@gmail.com>
2026-08-09 00:47:47 +0530

    implement Retriever

--- src/retriever.py
+++ src/retriever.py
@@ -1,5 +1,9 @@
 class Retriever:
-    def __init__():
+    def __init__(self, embedding_model, vector_store, top_k:int):
+        self.embedding_model = embedding_model
+        self.vector_store = vector_store
+        self.top_k = top_k
 
-    def retrieve():
-        
+    def retrieve(self, query:str) -> list[str]:
+        query_emb = self.embedding_model.encode([query])
+        return self.vector_store.search(query_emb, self.top_k)
